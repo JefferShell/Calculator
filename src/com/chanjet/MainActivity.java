34d8c207d581714d5625b9504a3ebf32fcbe8d53
JefferShell <1876993774@qq.com>
2014-12-01 21:44:38 +0800

    Signed-off-by: JefferShell <1876993774@qq.com>

---
+++ src/com/chanjet/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chanjet;
+
+import com.chanjet.calculator.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
